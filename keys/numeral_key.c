af0f693fa32ad60fc9063f41663315420a616728
AlexisDeLaCruzHernandez <103050852+AlexisDeLaCruzHernandez@users.noreply.github.com>
2024-12-13 22:33:04 -0300

    Update numeral_key.c

--- keys/numeral_key.c
+++ keys/numeral_key.c
@@ -15,6 +15,7 @@ void tecla_numeral (opcion_t *op, menu_t menu) {
 			toggle_op_d (menu);
 			break;
 		case op_numeral:
+			*op = ninguno;
 			if (cambio_de_config == 1) {
 				FLASH_Guardado (contraste, volumen, &version, angulo_alarma, distancia_alarma);
 				cambio_de_config = 0;
@@ -24,7 +25,6 @@ void tecla_numeral (opcion_t *op, menu_t menu) {
 					Delay_ms (250);
 					if (i != 4) INDICADOR_Enciendo (TIEMPO_INDICACION, 99);
 				}
-				*op = ninguno;
 				return;
 			}
 		}
